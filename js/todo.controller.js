956081c8378c59e2bdb7d6b2e0716b059a06f8d3
kakhavan <kian.akhavan@gmail.com>
2019-04-25 14:12:46 -0700

    first commit

---
+++ js/todo.controller.js
@@ -0,0 +1,16 @@
+function TodoController() {
+    this.list = [{
+        title: 'First todo item!',
+        completed: true
+    },{
+        title: 'Second todo item!',
+        completed: false
+    }, {
+        title: 'Third todo item!',
+        completed: true
+    },];
+}
+
+angular
+    .module("app")
+    .controller("TodoController", TodoController);
