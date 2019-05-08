17c58746133281d5d31a50e226af0683a7ec80e5
kakhavan <kian.akhavan@gmail.com>
2019-05-08 13:36:10 -0700

    Feature: add and remove todos to list

--- js/todo.controller.js
+++ js/todo.controller.js
@@ -1,4 +1,5 @@
 function TodoController() {
+    this.newTodo = '';
     this.list = [{
         title: 'First todo item!',
         completed: true
@@ -8,9 +9,23 @@ function TodoController() {
     }, {
         title: 'Third todo item!',
         completed: true
-    },];
-}
+    }];
+
+    this.addTodo = function() {
+        this.list.unshift({
+            title: this.newTodo,
+            completed: false
+        });
+        this.newTodo = '';
+    };
+
+    this.removeTodo = function(item, index) {
+        this.list.splice(index, 1);
+    };
+};
+
+
 
 angular
-    .module("app")
-    .controller("TodoController", TodoController);
+    .module('app')
+    .controller('TodoController', TodoController);
